7a9f961828e53669e8a41ed87cda613923f3f42f
Kyle G <twilger.kg20@gmail.com>
2018-02-04 23:12:01 -0500

    Uploading Full Prototype Scene
    Use mainScene.unity. Fixing object placement.

--- lights-assignment-proto/tankses-experiments-light/Assets/Scripts/Managers/TankManager.cs
+++ lights-assignment-proto/tankses-experiments-light/Assets/Scripts/Managers/TankManager.cs
@@ -16,7 +16,7 @@ namespace Complete
         public Color m_PlayerColor;                             // This is the color this tank will be tinted.
         public Transform m_SpawnPoint;                          // The position and direction the tank will have when it spawns.
         [HideInInspector] public int m_PlayerNumber;            // This specifies which player this the manager for.
-        [HideInInspector] public string m_ColoredPlayerText;    // A string that represents the player with their number colored to match their tank.
+        public string m_ColoredPlayerText;    // A string that represents the player with their number colored to match their tank.
         [HideInInspector] public GameObject m_Instance;         // A reference to the instance of the tank when it is created.
         [HideInInspector] public int m_Wins;                    // The number of wins this player has so far.
 		[HideInInspector] public List<Transform> m_WayPointList;
