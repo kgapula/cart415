1582dbe41268b3f9f743e1403648ca2e85f7ad3d
Kyle G <twilger.kg20@gmail.com>
2018-04-21 19:09:01 -0400

    Commit before creating new Final Project branch

---
+++ week1_TanksTutorial/UniteBoston2015TrainingDay/Assets/LightResetScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightResetScript : MonoBehaviour {
+
+	public Light DirectionalLight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	void OnTriggerEnter (Collider other) {
+
+		//if the block was hit by a shell
+		if (other.gameObject.tag == "Shell") {
+			Debug.Log ("Light reset object was hit");
+
+			DirectionalLight.color = Color.white;
+		}
+	}
+}
---
+++ week1_TanksTutorial/UniteBoston2015TrainingDay/Assets/LightScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+	public Light DirectionalLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter (Collider other) {
+
+		//if the block was hit by a shell
+		if (other.gameObject.tag == "Shell") {
+			Debug.Log ("Light object was hit");
+
+			DirectionalLight.color = Random.ColorHSV (0f, 1f, 1f, 1f, 0.5f, 1f);
+		}
+	}
+}
